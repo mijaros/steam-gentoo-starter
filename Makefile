4511e135c77cafea1803e48084bf7997e3d3ad2c
Miroslav Jaros <mirek@mijaros.cz>
2021-11-11 22:27:06 +0100

    Add desktop file and extend runner to forward arguments

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ steam: main.o
 		$(CC) -o steam main.o
 
 main.o: main.c
-		$(CC) $(CFLAGS) -c main.c
+		$(CC) $(CFLAGS) -DTARGET_DIR=$(TARGET_DIR) -c main.c
 
 clean:
 		rm *.o steam
@@ -17,9 +17,12 @@ install: steam steam-chroot
 		install -d $(TARGET_DIR)
 		install -s -g root -m 6111 steam -t $(TARGET_DIR)
 		install -g root -m 755 steam-chroot -t $(TARGET_DIR)
+		desktop-file-install --dir /usr/local/share/applications --rebuild-mime-info-cache --remove-key Icon --set-key Exec --set-value '$(TARGET_DIR)/steam %U' steam.desktop
 
 uninstall:
 		rm -f $(TARGET_DIR)/steam
 		rm -f $(TARGET_DIR)/steam-chroot
+		rm -f /usr/local/share/applications/steam.desktop
+		update-desktop-database
 
 .PHONY: all clean uninstall install
--- main.c
+++ main.c
@@ -5,17 +5,34 @@
 #include <string.h>
 #include <errno.h>
 
-#define CHROOT_SCRIPT "/usr/local/bin/steam-chroot"
+#define STR(A) #A
+#define EVAL_STR(A) STR(A)
 
-int main()
+#define SCRIPT_NAME "/steam-chroot"
+
+#ifndef TARGET_DIR
+#error "TARGET_DIR macro not specified"
+#endif
+
+#define TARGET_DIR_STR EVAL_STR(TARGET_DIR)
+#define CHROOT_SCRIPT TARGET_DIR_STR SCRIPT_NAME
+
+int main(int argc, char *argv[])
 {
     uid_t uid = geteuid();
     if (setuid(uid)) {
         fprintf(stderr, "setuid failed: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
-    if (execl(CHROOT_SCRIPT, CHROOT_SCRIPT)) {
-        fprintf(stderr, "execl failed: %s\n", strerror(errno));
+
+    char *exec_argv[argc+1]; //Make space for past-the-end NULL
+    exec_argv[0] = CHROOT_SCRIPT; //Change argv[0] to correct value
+    for (int i = 1; i <= argc; i++) { // Copy including NULL
+        exec_argv[i] = argv[i];
+    }
+
+    if (execvp(CHROOT_SCRIPT, exec_argv)) {
+        fprintf(stderr, "execvp failed: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
